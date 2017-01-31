13f6eecef012f89e5f9bba65597971444c0c48e1
Hanno <hanno@gentoo.org>
2017-01-31 10:46:59 +0100

    add more bug links

--- pcre-backtracking.php
+++ pcre-backtracking.php
@@ -4,8 +4,10 @@
  * Whether this crashes depends on several circumstances, including the version, configuration
  * and whether libpcre was compiled with jit support.
  *
- * This has been reported to PHP:
- * - https://bugs.php.net/bug.php?id=71720 (closed as "Not a bug")
+ * This has been reported to PHP multiple times (all closed as "Not a bug"):
+ * - https://bugs.php.net/bug.php?id=45735 (this has the most detailed explanation)
+ * - https://bugs.php.net/bug.php?id=61744
+ * - https://bugs.php.net/bug.php?id=71720
  */
 
 preg_match("/(0)*/", str_repeat("0", 15000));
